52ddc88681cdea6fc5562ae444f8e3f3eba7d384
Rohan.27s <rohan.gotwal@graphia.in>
2022-12-20 23:50:34 +0530

    society details page added

--- src/partials/Home/Header/index.jsx
+++ src/partials/Home/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => (
   <header className='home-header' style={{marginTop:"10px"}}>
     <h2>Presenting the societies of</h2>
     <h1>
-      <span>“</span> Mahraja Surajmal Institute <span>”</span>
+      <span>“</span> Maharaja Surajmal Institute <span>”</span>
     </h1>
   </header>
 );
