4dbec43474fc5b6cb3117d20af9bb3a664bfc349
Rohan.27s <rohan.gotwal@graphia.in>
2022-12-21 00:04:43 +0530

    society details page added

--- src/partials/config/data.js
+++ src/partials/config/data.js
@@ -11,19 +11,19 @@ export const blogList = [
   {
     id: 'Synergy',
     title: 'Synergy',
-    category: 'Music',
+    category: 'Commerce',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     authorName: 'John Doe',
-    cover: 'https://res.cloudinary.com/rohangotwal/image/upload/v1671559670/Blog/131210953_905343756870234_1122747852493756031_n_zb7gvd.jpg',
+    cover: 'https://res.cloudinary.com/rohangotwal/image/upload/v1671561045/Blog/285990370_178786201253846_6543112328762093762_n_rafdeb.jpg',
   },
   {
     id: 'NSS',
     title: 'NSS',
-    category: 'Social',
+    category: 'Cultural',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     authorName: 'John Doe',
-    cover: 'https://res.cloudinary.com/rohangotwal/image/upload/v1671559670/Blog/131210953_905343756870234_1122747852493756031_n_zb7gvd.jpg',
+    cover: 'https://res.cloudinary.com/rohangotwal/image/upload/v1671560957/Blog/305589116_1104800217091785_8428007337901629851_n_eyx8cc.jpg',
   },
 ];
