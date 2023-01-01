ae3027b8612d88ed91fe77c6c5ffddccf1ff1bfc
Rohan.27s <rohan.gotwal@graphia.in>
2023-01-01 20:28:56 +0530

    add more button added

--- src/partials/admin/RegisterEvent.jsx
+++ src/partials/admin/RegisterEvent.jsx
@@ -27,13 +27,22 @@ const RegisterEvent = () => {
     console.log(details);
   }
   const Field = () => {
-    return <><h3>Society Name:</h3><input type="text" name="content" placeholder='Enter the names of the society' onChange={handleChange} required />
-      <h3>Logo:</h3><input type="text" name="content" placeholder='Enter the url of logo of the society' onChange={handleChange} required /></>
+    return <><h3>Society Name:</h3><input type="text" name="Name" placeholder='Enter the names of the society' onChange={handleChange} required />
+      <h3>Logo:</h3><input type="text" name="Logo" placeholder='Enter the url for logo of the society' onChange={handleChange} required /></>
+  }
+  const Faq = () => {
+    return <><h3>Question:</h3><input type="text" name="question" placeholder='Enter the question' onChange={handleChange} required />
+      <h3>Answer:</h3><input type="text" name="answer" placeholder='Enter the answer for the above question' onChange={handleChange} required /></>
   }
   const [societies, setSocieties] = useState([])
-  const addMore = event => {
+  const [faqs, setFaqs] = useState([])
+
+  const addMore = () => {
     setSocieties(societies.concat(<Field />));
     console.log(societies);
+  }
+  const addMorefaq = () => {
+    setFaqs(faqs.concat(<Faq />));
   }
   return (
     <div className='register-event'>
@@ -44,8 +53,8 @@ const RegisterEvent = () => {
         <h2>Names of the Societies with their logo:</h2>
         <a className='addMore' onClick={addMore}>Add more</a>
 
-        <h3>Society Name:</h3><input type="text" name="content" placeholder='Enter the names of the society' onChange={handleChange} required />
-        <h3>Logo:</h3><input type="text" name="content" placeholder='Enter the url of logo of the society' onChange={handleChange} required />
+        <h3>Society Name:</h3><input type="text" name="Name" placeholder='Enter the names of the society' onChange={handleChange} required />
+        <h3>Logo:</h3><input type="text" name="Logo" placeholder='Enter the url for logo of the society' onChange={handleChange} required />
         {societies}
         <h3>Description:</h3><textarea type="text" name="description" placeholder='Enter the description of the society' onChange={handleChange} required />
         <h3>Event Date:</h3><input type="text" name="date" placeholder='Enter the date of the event' onChange={handleChange} required />
@@ -54,14 +63,11 @@ const RegisterEvent = () => {
         <h3>Event Poster:</h3><input type="text" name="img" placeholder='Enter the url for the poster of the event' onChange={handleChange} required />
 
         <h2>FAQ Section</h2>
-        <h3>Question 1:</h3><input type="text" name="img" placeholder='Enter the first question' onChange={handleChange} required />
-        <h3>Answer 1:</h3><input type="text" name="img" placeholder='Enter the answer for the above question' onChange={handleChange} required />
+        <a className='addMore' onClick={addMorefaq}>Add more</a>
 
-        <h3>Question 2:</h3><input type="text" name="img" placeholder='Enter the second question' onChange={handleChange} />
-        <h3>Answer 2:</h3><input type="text" name="img" placeholder='Enter the answer for the above question' onChange={handleChange} />
-
-        <h3>Question 3:</h3><input type="text" name="img" placeholder='Enter the third question' onChange={handleChange} />
-        <h3>Answer 3:</h3><input type="text" name="img" placeholder='Enter the answer for the above question' onChange={handleChange} />
+        <h3>Question:</h3><input type="text" name="question" placeholder='Enter the  question' onChange={handleChange} required />
+        <h3>Answer:</h3><input type="text" name="answer" placeholder='Enter the answer for the above question' onChange={handleChange} required />
+        {faqs}
         <div><button type="submit" id="submit-form">Submit</button></div>
       </form>
     </div>
