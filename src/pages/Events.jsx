f5f7389fee6f923f42afe2e192e21b9bb66c451c
Rohan.27s <rohan.gotwal@graphia.in>
2023-01-03 13:18:18 +0530

    backend merged

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -2,26 +2,40 @@ import React from 'react'
 import Header from '../partials/Header';
 import CardComponent from '../partials/CardComponent';
 import PastCardComponent from '../partials/PastCardComponent';
-import { upcomingEvent } from '../partials/config/event';
 import { useState } from 'react';
 const Events = () => {
-  let currCards = <CardComponent />;
+  const [isActive, setActive] = useState(false);
+  const [isActive1, setActive1] = useState(true);
+
+  
   const [eventType, setEventType] = useState(<CardComponent />)
+  const handleClick =()=>{
+    setEventType(<PastCardComponent />);
+     setActive(true)
+     setActive1(false)
+
+  }
+  const handleClick1 =()=>{
+    setEventType(<CardComponent />);
+    setActive1(true)
+    setActive(false)
+
+  }
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       <Header />
-      <div className="cards">       
-     
+      <div className="cards">
+
         {eventType}
         <div className="bg-white backdrop-blur-sm shadow-lg bottombar">
           <li>
-            <button onClick={() => setEventType(<PastCardComponent />)}>Past Events</button>
-          </li>
-           |
-          <li> <button onClick={() => setEventType(<CardComponent />)}>Upcoming Events</button>   </li>
-        </div>
+            <button className={isActive ? "active" : null} onClick={handleClick} >Past Events</button>
+        </li>
+        |
+        <li> <button className={isActive1 ? "active" : null} onClick={handleClick1}>Upcoming Events</button>   </li>
       </div>
     </div>
+    </div >
   )
 }
 
