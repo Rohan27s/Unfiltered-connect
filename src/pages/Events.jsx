2e1ea5fa307c9453ed5b248592c0ac8a278f1d41
Rohan.27s <rohan.gotwal@graphia.in>
2023-01-08 19:29:10 +0530

    css fix

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -2,40 +2,45 @@ import React from 'react'
 import Header from '../partials/Header';
 import CardComponent from '../partials/CardComponent';
 import PastCardComponent from '../partials/PastCardComponent';
+import Footer from '../partials/Footer';
+
 import { useState } from 'react';
 const Events = () => {
   const [isActive, setActive] = useState(false);
-  const [isActive1, setActive1] = useState(true);  
+  const [isActive1, setActive1] = useState(true);
   const [eventType, setEventType] = useState(<CardComponent />)
   //Underlining the selected nav links
-  const handleClick =()=>{
+  const handleClick = () => {
     setEventType(<PastCardComponent />);
-     setActive(true)
-     setActive1(false)
+    setActive(true)
+    setActive1(false)
 
   }
   //Underlining the selected nav links
-  const handleClick1 =()=>{
+  const handleClick1 = () => {
     setEventType(<CardComponent />);
     setActive1(true)
     setActive(false)
 
   }
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      <Header />
-      <div className="cards">
+    <>
+      <div className="flex flex-col min-h-screen overflow-hidden" style={{ paddingBottom: "5%" }}>
+        <Header />
+          <div className="bg-white backdrop-blur-sm shadow-lg bottombar">
+            <li>
+              <button className={isActive ? "active" : null} onClick={handleClick} >Past Events</button>
+            </li>
+            <li>|</li>
+            <li> <button className={isActive1 ? "active" : null} onClick={handleClick1}>Upcoming Events</button>   </li>
+          </div>
+        <div className="cards">
+          {eventType}
+        </div>
 
-        {eventType}
-        <div className="bg-white backdrop-blur-sm shadow-lg bottombar">
-          <li>
-            <button className={isActive ? "active" : null} onClick={handleClick} >Past Events</button>
-        </li>
-        <li>|</li>
-        <li> <button className={isActive1 ? "active" : null} onClick={handleClick1}>Upcoming Events</button>   </li>
-      </div>
-    </div>
-    </div >
+      </div >
+      <Footer />
+    </>
   )
 }
 
