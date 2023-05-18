4584e2e8895b743a062b50abcad75c292461d391
Rohan27s <rohangotwal3@gmail.com>
2023-05-18 21:10:17 +0530

    societies sorted in alphabatical order

--- src/partials/Home/SocietyList/index.jsx
+++ src/partials/Home/SocietyList/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import SocietyItem from './SocietyItem';
 import './styles.css';
 import EmptyList from '../../common/EmptyList';
+
 const SocietyList = ({ searchResult }) => {
-  const results=searchResult.map((society) => (
-    <SocietyItem blog={society} key={society.name}/>
-  ))
-  const content = results?.length ? results : <EmptyList />
+  const sortedResults = searchResult.sort((a, b) => a.name.localeCompare(b.name));
+  
+  const results = sortedResults.map((society) => (
+    <SocietyItem blog={society} key={society.name} />
+  ));
+
+  const content = results?.length ? results : <EmptyList />;
+  
   return (
     <div className='blogList-wrap'>
       {content}
