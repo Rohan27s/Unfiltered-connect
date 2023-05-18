bfa4c867d247bdbcddd522534c1640780fca1fca
Rohan27s <rohangotwal3@gmail.com>
2023-05-18 21:20:12 +0530

    events sorted by date

--- src/partials/CardComponent.jsx
+++ src/partials/CardComponent.jsx
@@ -1,63 +1,69 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import Loading from "./Loading";
+
 export default function CardComponent() {
-    const [loading, setLoading] = useState(true);
-    const [post, setPost] = useState([]);
-    //API call for getting info about all the events
-    useEffect(() => {
-        axios.get('https://unfiltered-connect-backend.vercel.app/api/allevent')
-          .then((response) => {
-            // Filter the posts based on the date
-            const currentDate = new Date();
-            const filteredPosts = response.data.filter(item => {
-              const postDate = new Date(item.date.split("-").reverse().join("-")); // Convert string date to Date object
-              return postDate >= currentDate;
-            });
-            setPost(filteredPosts);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.error(error);
-            setLoading(false);
-          });
-      }, []);
-    
-    return (
-        <>
-            {loading ? <Loading/> :
-                <section className="relative event-card-compo" >
-                    <div style={{ marginTop: "15%" }} className="grid gap-8 lg:grid-cols-4">
+  const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState([]);
 
-                        {post.map((items, key) => (
-                                    <a href={`/event-details/${items._id}`} >
+  // API call for getting info about all the events
+  useEffect(() => {
+    axios.get('https://unfiltered-connect-backend.vercel.app/api/allevent')
+      .then((response) => {
+        const currentDate = new Date();
+        
+        // Filter and sort the events
+        const filteredEvents = response.data.filter(item => {
+          const postDate = new Date(item.date.split("-").reverse().join("-"));
+          return postDate >= currentDate;
+        });
 
-                            <div className="eventcards w-full rounded-lg shadow-md lg:max-w-sm" key={items.title}>
-                                <img
-                                    className="object-cover w-full h-60"
-                                    src={items.img}
-                                    alt="image"
-                                />
-                                <div className="p-4">
-                                    <h4 className="text-xl font-semibold text-blue-600">
+        const sortedEvents = filteredEvents.sort((a, b) => {
+          const dateA = new Date(a.date.split("-").reverse().join("-"));
+          const dateB = new Date(b.date.split("-").reverse().join("-"));
+          return dateA - dateB;
+        });
 
-                                        {items.title}
-                                    </h4>
-                                    <p className="mb-2 leading-normal">
-                                        {items.content}
-                                    </p>
-                                    <p className="mb-2 leading-normal font-semibold">
-                                        Date: {items.date}
-                                    </p>
-                                        
-                                </div>
-                            </div>
-                            </a>
+        setPost(sortedEvents);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
 
-                        ))}
-                    </div>
-                </section>}
-        </>
-    );
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <section className="relative event-card-compo">
+          <div style={{ marginTop: "15%" }} className="grid gap-8 lg:grid-cols-4">
+            {post.map((items, key) => (
+              <a href={`/event-details/${items._id}`} key={items.title}>
+                <div className="eventcards w-full rounded-lg shadow-md lg:max-w-sm">
+                  <img
+                    className="object-cover w-full h-60"
+                    src={items.img}
+                    alt="image"
+                  />
+                  <div className="p-4">
+                    <h4 className="text-xl font-semibold text-blue-600">
+                      {items.title}
+                    </h4>
+                    <p className="mb-2 leading-normal">{items.content}</p>
+                    <p className="mb-2 leading-normal font-semibold">
+                      Date: {items.date}
+                    </p>
+                  </div>
+                </div>
+              </a>
+            ))}
+          </div>
+        </section>
+      )}
+    </>
+  );
 }
