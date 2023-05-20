cdcac163da52c3f7cca1831afe5c3637ab6cd57b
Rohan27s <rohangotwal3@gmail.com>
2023-05-20 16:00:29 +0530

    society admin merged

--- src/partials/admin/RegisterSociety.jsx
+++ src/partials/admin/RegisterSociety.jsx
@@ -1,97 +1,209 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
+
 const RegisterSociety = () => {
   const formRef = useRef(null);
+  const [societyData, setSocietyData] = useState({
+    name: '',
+    category: '',
+    description: '',
+    cover: '',
+    members: [
+      {
+        designame: '',
+        desigholder: '',
+        number: '',
+        rank: 0,
+      },
+    ],
+    url1: '',
+    url2: '',
+    url3: '',
+  });
 
-  const [details, setDetails] = useState({
-    name: "",
-    category: "",
-    description: "",
-    cover: "",
-    President: "",
-    VicePresident: "",
-    GeneralSecretary: "",
-    EventsHead: "",
-    DesignHead: "",
-    PRandOutreachHead: "",
-    ContentHead: "",
-    url1:"",
-    url2:"",
-    url3:"",
-  })
-  const handleChange = (e) => {
+  const handleInputChange = (e, index, field, subField) => {
     const { name, value } = e.target;
-    setDetails({
-      ...details,
-      [name]: value,
+    const updatedSocietyData = { ...societyData };
+
+    if (subField) {
+      if (!updatedSocietyData[field]) {
+        updatedSocietyData[field] = [{ [subField]: value }];
+      } else if (index >= 0 && index < updatedSocietyData[field].length) {
+        updatedSocietyData[field][index][subField] = value;
+      }
+    } else {
+      if (index >= 0 && index < updatedSocietyData[field].length) {
+        updatedSocietyData[field][index][name] = value;
+      } else {
+        updatedSocietyData[name] = value;
+      }
+    }
+
+    setSocietyData(updatedSocietyData);
+  };
+
+  const handleAddField = (field) => {
+    const updatedSocietyData = { ...societyData };
+    if (!updatedSocietyData[field]) {
+      updatedSocietyData[field] = [];
+    }
+    updatedSocietyData[field].push({
+      designame: '',
+      desigholder: '',
+      number: '',
+      rank: 0,
     });
-  }
+    setSocietyData(updatedSocietyData);
+  };
+
+  const handleRemoveField = (index, field) => {
+    const updatedSocietyData = { ...societyData };
+    updatedSocietyData[field].splice(index, 1);
+    setSocietyData(updatedSocietyData);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // API call for adding society in our mongo db
+    // Save the societyData to the database or perform any other necessary actions
+    console.log(societyData);
     var config = {
       method: 'post',
       url: 'https://unfiltered-connect-backend.vercel.app/api/societyadd',
-      data : details
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: societyData,
     };
-    
-    axios(config)
-    .then(function (response) {
-      console.log(details);
-      alert("Society Added Successfully");
-      setDetails({
-        name: "",
-        category: "",
-        description: "",
-        cover: "",
-        President: "",
-        VicePresident: "",
-        GeneralSecretary: "",
-        EventsHead: "",
-        DesignHead: "",
-        PRandOutreachHead: "",
-        ContentHead: "",
-        url1:"",
-        url2:"",
-        url3:"",
-      });
-      formRef.current.reset();
-    })
-    .catch(function (error) {
-      alert("Error! Try Again");
-    });
-    
-  }
-  return (
-    <div className='register-event'>
-      <h1>Register Society</h1>
-      <form onSubmit={handleSubmit} ref={formRef}>
-        <h3>Society Name: <p style={{color:'red',display:'inline'}}>*</p></h3><input type="text" name="name" placeholder='Please enter the name of the society' onChange={handleChange} required />
-        <h3>Category: <p style={{color:'red',display:'inline'}}>*</p></h3><input type="text" name="category" placeholder='Please enter the category of the society' onChange={handleChange} required />
-        <h3>Description: <p style={{color:'red',display:'inline'}}>*</p></h3><textarea type="text" name="description" placeholder='Please enter the description about the society' onChange={handleChange} required />
-        <h3>Logo: <p style={{color:'red',display:'inline'}}>*</p></h3><input type="text" name="cover" placeholder='Please enter the url of the logo' onChange={handleChange} required />
-       
-       
-        
-        <div className="event-winner-head">
-          <h2>Core Members (with their contact number)</h2>
-          <button >Add more</button>
-        </div>
-        <div className="pos-holders">
-          <h3>Designation Name</h3><input type="text" placeholder='Please enter the name of the Designation' />
-          <h3>Designation Holder</h3><input placeholder='Please enter the name of Designation holder' />
-          <h3>Mobile Number</h3><input placeholder='Please enter the name of Designation holder' />
 
-          {/* {positions} */}
+    axios(config)
+      .then(function (response) {
+        alert('Society Registered Successfully');
+        setSocietyData({
+          name: '',
+          category: '',
+          description: '',
+          cover: '',
+          members: [
+            {
+              designame: '',
+              desigholder: '',
+              number: '',
+              rank: 0,
+            },
+          ],
+          url1: '',
+          url2: '',
+          url3: '',
+        });
+        formRef.current.reset();
+      })
+      .catch(function (error) {
+        alert('Error! Please Try Again');
+      });
+  };
+
+  return (
+    <div className="register-event">
+      <h1>Register a Society</h1>
+
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <h3>Society Name:<p style={{color:'red',display:'inline'}}>*</p></h3>
+        <input
+          type="text"
+          name="name"
+          value={societyData.name}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+
+        <h3>Society Category:<p style={{color:'red',display:'inline'}}>*</p></h3>
+        <input
+          type="text"
+          name="category"
+          value={societyData.category}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+
+        <h3>Description:<p style={{color:'red',display:'inline'}}>*</p></h3>
+        <textarea
+          name="description"
+          value={societyData.description}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+
+        <h3>Cover Image URL:<p style={{color:'red',display:'inline'}}>*</p></h3>
+        <input
+          type="text"
+          name="cover"
+          value={societyData.cover}
+          onChange={handleInputChange}
+        />
+        <br />
+        <div className="event-winner-head">
+
+        <h2>Member Details:<p style={{color:'red',display:'inline'}}>*</p></h2>   <button type="button" className='addSocbtn' onClick={() => handleAddField('members')}>
+          Add Member
+        </button>
         </div>
-        <h2>Photo Gallery Section </h2>
-        <h3>Image 1: <p style={{color:'red',display:'inline'}}>*</p></h3><input  type="text" name="url1" placeholder='Please enter the image url' onChange={handleChange} required />
-        <h3>Image 2: <p style={{color:'red',display:'inline'}}>*</p></h3><input  type="text" name="url2" placeholder='Please enter the image url' onChange={handleChange} required />
-        <h3>Image 3: <p style={{color:'red',display:'inline'}}>*</p></h3><input  type="text" name="url3" placeholder='Please enter the image url' onChange={handleChange} required />
-        <div><button type="submit" id="submit-form">Submit</button></div>
+        {societyData.members.map((member, index) => (
+          <div key={`member-${index}`} className="society-names">
+           <h3>Designation Name({index+1}): <p style={{ color: 'red', display: 'inline' }}>*</p></h3> <input
+              type="text"
+              name="designame"
+              placeholder="Designation Name"
+              value={member.designame}
+              onChange={(e) =>
+                handleInputChange(e, index, 'members', 'designame')
+              }
+            />
+            <h3>Designation Holder's Name({index+1}): <p style={{ color: 'red', display: 'inline' }}>*</p></h3><input
+              type="text"
+              name="desigholder"
+              placeholder="Designation Holder"
+              value={member.desigholder}
+              onChange={(e) =>
+                handleInputChange(e, index, 'members', 'desigholder')
+              }
+            />
+            <h3>Mobile Number({index+1}): <p style={{ color: 'red', display: 'inline' }}>*</p></h3><input
+              type="text"
+              name="number"
+              placeholder="Number"
+              value={member.number}
+              onChange={(e) => handleInputChange(e, index, 'members', 'number')}
+            />
+            <h3>Member's Rank({index+1}): <p style={{ color: 'red', display: 'inline' }}>*</p></h3><input
+              type="number"
+              name="rank"
+              placeholder="Rank"
+              value={member.rank}
+              onChange={(e) => handleInputChange(e, index, 'members', 'rank')}
+            />
+            {index > 0 && (
+              <button
+                type="button"
+                className='remove-btn'
+                onClick={() => handleRemoveField(index, 'members')}
+              >
+                Remove
+              </button>
+            )}
+          </div>
+        ))}
+     
+        <br />
+        
+        
+
+        <button type="submit"id="submit-form">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterSociety
+export default RegisterSociety;
