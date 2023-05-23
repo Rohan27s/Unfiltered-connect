1dc4dd7fb29238b665907ea231ed1e42751c9dc0
Rohan27s <rohangotwal3@gmail.com>
2023-05-23 09:06:46 +0530

    bug sorted

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,80 +1,82 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import RegisterEvent from '../partials/admin/RegisterEvent';
 import RegisterSociety from '../partials/admin/RegisterSociety';
 import Default from '../partials/admin/Default';
-import { useNavigate } from "react-router-dom";
 import RegisterPastEvent from '../partials/admin/RegisterPastEvent';
 import Header from '../partials/Header';
 import Footer from '../partials/Footer';
-import { Link } from 'react-router-dom';
+
 const Admin = () => {
   let navigate = useNavigate();
   const [isActive, setActive] = useState(false);
   const [isActive1, setActive1] = useState(false);
   const [isActive2, setActive2] = useState(false);
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false); // Track the state of sidebar collapse
 
-  //Redirect to admin login
+  // Redirect to admin login
   const handleLogout = () => {
     let path = `/admin`;
     navigate(path);
   }
-  //Set Underline under active headings
+
+  // Set Underline under active headings
   const handleClick = () => {
     setCurr(<RegisterEvent />)
     setActive(true)
     setActive1(false)
     setActive2(false)
   }
+
   const handleClick1 = () => {
     setCurr(<RegisterPastEvent />)
     setActive(false)
     setActive1(true)
     setActive2(false)
-
   }
+
   const handleClick2 = () => {
     setCurr(<RegisterSociety />)
     setActive(false)
     setActive1(false)
     setActive2(true)
-
   }
+
   const [curr, setCurr] = useState(<Default />);
+
+  const toggleSidebar = () => {
+    setSidebarCollapsed(!sidebarCollapsed);
+  }
+
   return (
     <>
-      <div className='main-admin'>
-        {/* <Header className="head-ad
-        min" /> */}
+      <div className={`main-admin ${sidebarCollapsed ? 'collapsed' : ''}`}>
         <div className="admin-nav">
           <div className="top-admin-nav">
             {/* Logo */}
             <Link to="/" className="block logo" aria-label="Cruip">
               <img className='nav-logo' src="https://res.cloudinary.com/rohangotwal/image/upload/v1671085611/Blog/logo_cy14jc.png" alt="" />
-              <h2>Unfiltered Connect</h2>
+              {sidebarCollapsed ? "" : <h2>Unfiltered Connect</h2>}
             </Link>
           </div>
-          <ul className='mid-admin-nav'>
+          <ul className={`mid-admin-nav ${sidebarCollapsed ? 'collapsed' : ''}`}>
             <li><button className={isActive ? "active" : null} onClick={handleClick}>Register New Event</button></li>
-            {/* <li>|</li> */}
             <li><button className={isActive1 ? "active" : null} onClick={handleClick1}>Report Past Event</button></li>
-            {/* <li>|</li> */}
-
             <li><button className={isActive2 ? "active" : null} onClick={handleClick2}>Register Society</button></li>
           </ul>
           <div className="bottom-nav-admin">
-
-            <button className="logout" onClick={handleLogout}>Logout</button>
+            <button className={`mid-admin-nav ${sidebarCollapsed ? 'logout collapsed' : 'logout'}`} onClick={handleLogout}>Logout</button>
           </div>
+          <button className={`sidebar-toggle ${sidebarCollapsed ? 'collapsed' : ''}`} onClick={toggleSidebar}>
+            {sidebarCollapsed?"Show":"Hide" }
+          </button>
         </div>
         <div className="admin-container">
           {curr}
         </div>
       </div>
-      {/* <Footer/> */}
-
     </>
   )
 }
 
-export default Admin
+export default Admin;
